21ed150dd5a5d59a8bdbfa4bc9ecb9edfece7c76
Ben <bdhsu@berkeley.edu>
2020-03-04 13:18:46 -0800

    permissions no longer required. sacrifices title

--- Sources/active-win/main.swift
+++ Sources/active-win/main.swift
@@ -1,30 +1,30 @@
 import AppKit
 
-func getActiveBrowserTabURLAppleScriptCommand(_ appName: String) -> String? {
-	switch appName {
-	case "Google Chrome", "Brave Browser", "Microsoft Edge":
-		return "tell app \"\(appName)\" to get the URL of active tab of front window"
-	case "Safari":
-		return "tell app \"Safari\" to get URL of front document"
-	default:
-		return nil
-	}
-}
+// func getActiveBrowserTabURLAppleScriptCommand(_ appName: String) -> String? {
+// 	switch appName {
+// 	case "Google Chrome", "Brave Browser", "Microsoft Edge":
+// 		return "tell app \"\(appName)\" to get the URL of active tab of front window"
+// 	case "Safari":
+// 		return "tell app \"Safari\" to get URL of front document"
+// 	default:
+// 		return nil
+// 	}
+// }
 
 // Show accessibility permission prompt if needed. Required to get the complete window title.
-if !AXIsProcessTrustedWithOptions(["AXTrustedCheckOptionPrompt": true] as CFDictionary) {
-	print("active-win requires the accessibility permission in “System Preferences › Security & Privacy › Privacy › Accessibility”.")
-	exit(1)
-}
+// if !AXIsProcessTrustedWithOptions(["AXTrustedCheckOptionPrompt": true] as CFDictionary) {
+// 	print("active-win requires the accessibility permission in “System Preferences › Security & Privacy › Privacy › Accessibility”.")
+// 	exit(1)
+// }
 
 let frontmostAppPID = NSWorkspace.shared.frontmostApplication!.processIdentifier
 let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly, .excludeDesktopElements], kCGNullWindowID) as! [[String: Any]]
 
 // Show screen recording permission prompt if needed. Required to get the complete window title.
-if !hasScreenRecordingPermission() {
-	print("active-win requires the screen recording permission in “System Preferences › Security & Privacy › Privacy › Screen Recording”.")
-	exit(1)
-}
+// if !hasScreenRecordingPermission() {
+// 	print("active-win requires the screen recording permission in “System Preferences › Security & Privacy › Privacy › Screen Recording”.")
+// 	exit(1)
+// }
 
 for window in windows {
 	let windowOwnerPID = window[kCGWindowOwnerPID as String] as! Int
@@ -54,7 +54,7 @@ for window in windows {
 	let appName = window[kCGWindowOwnerName as String] as! String
 
 	var dict: [String: Any] = [
-		"title": window[kCGWindowName as String] as? String ?? "",
+		// "title": window[kCGWindowName as String] as? String ?? "",
 		"id": window[kCGWindowNumber as String] as! Int,
 		"bounds": [
 			"x": bounds.origin.x,
@@ -72,12 +72,12 @@ for window in windows {
 	]
 
 	// Only run the AppleScript if active window is a compatible browser.
-	if
-		let script = getActiveBrowserTabURLAppleScriptCommand(appName),
-		let url = runAppleScript(source: script)
-	{
-		dict["url"] = url
-	}
+	// if
+	// 	let script = getActiveBrowserTabURLAppleScriptCommand(appName),
+	// 	let url = runAppleScript(source: script)
+	// {
+	// 	dict["url"] = url
+	// }
 
 	print(try! toJson(dict))
 	exit(0)
